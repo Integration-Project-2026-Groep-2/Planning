9ab3931785c7cde0edd3fa7d9f62c3ea6bc6e7b0
adyousfi <adamyousfi789@gmail.com>
2026-03-27 15:17:19 +0100

    RabbitMQ queue assertion
    RabbitMQ queue assertion toegevoegd

--- src/producers/heartbeat.producer.ts
+++ src/producers/heartbeat.producer.ts
@@ -7,6 +7,9 @@ export const startHeartbeatProducer = () => {
 const sendHeartbeat = async () => {
   try {
     const channel = getChannel();
+    const queueName = 'planning.heartbeat';
+
+    await channel.assertQueue(queueName, { durable: true });
     
   } catch (error) {
     console.error('[Heartbeat] Fout bij het verzenden:', error);
